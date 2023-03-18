7e21b16b81f1f55cc598e44016c5d51f1544a2cc
Rafael Heitor <rafaelheitor2@outlook.com>
2023-03-18 14:19:05 -0300

    feat: Adding Abstract Factory pattern

---
+++ src/POO/Design_Patterns/Creational/Abstract Factory/AbstractFactory-example.ts
@@ -0,0 +1,134 @@
+interface IChair {
+  type: string;
+  dimensions: dimensions;
+  getDimensions(): dimensions;
+}
+
+type dimensions = {
+  width: number;
+  height: number;
+  length?: number;
+  depth?: number;
+};
+
+interface ITable {
+  type: string;
+  dimensions: dimensions;
+  getDimensions(): string;
+  attachChair(chair: IChair): string;
+}
+
+interface AbstractKitchenFactory {
+  createChair(): IChair;
+  createTable(): ITable;
+}
+
+class ConcreteModernFactory implements AbstractKitchenFactory {
+  public createChair(): IChair {
+    return new ConcreteModernChair();
+  }
+
+  public createTable(): ITable {
+    return new ConcreteModernTable();
+  }
+}
+
+class ConcreteRusticFactory implements AbstractKitchenFactory {
+  public createChair(): IChair {
+    return new ConcreteRusticChair();
+  }
+
+  public createTable(): ITable {
+    return new ConcreteRusticTable();
+  }
+}
+
+class ConcreteModernChair implements IChair {
+  public type: string;
+  public dimensions: dimensions;
+
+  constructor() {
+    this.type = "Modern";
+    this.dimensions = {
+      height: 60,
+      width: 60,
+      depth: 60,
+    };
+  }
+  public getDimensions(): dimensions {
+    return this.dimensions;
+  }
+}
+
+class ConcreteModernTable implements ITable {
+  public type: string;
+  public dimensions: dimensions;
+
+  constructor() {
+    this.type = "Modern";
+    this.dimensions = {
+      height: 29,
+      width: 30,
+      length: 45.3,
+    };
+  }
+
+  public getDimensions(): string {
+    return `${JSON.stringify(this.dimensions)}`;
+  }
+
+  public attachChair(chair: IChair): string {
+    return `${chair.type} chair was attached. You can sit on it`;
+  }
+}
+
+class ConcreteRusticChair implements IChair {
+  public type: string;
+  public dimensions: dimensions;
+
+  constructor() {
+    this.type = "Rustic";
+    this.dimensions = {
+      height: 60,
+      width: 60,
+      depth: 60,
+    };
+  }
+  public getDimensions(): dimensions {
+    return this.dimensions;
+  }
+}
+
+class ConcreteRusticTable implements ITable {
+  public type: string;
+  public dimensions: dimensions;
+
+  constructor() {
+    this.type = "Rustic";
+    this.dimensions = {
+      height: 29,
+      width: 30,
+      length: 45.3,
+    };
+  }
+
+  public getDimensions(): string {
+    return `${JSON.stringify(this.dimensions)}`;
+  }
+
+  public attachChair(chair: IChair): string {
+    return `${chair.type} chair was attached. You can sit on it`;
+  }
+}
+
+function clientAbstractTableAndChairFactory(factory: AbstractKitchenFactory) {
+  const chair = factory.createChair();
+  const table = factory.createTable();
+
+  console.log(chair.getDimensions());
+  console.log(table.getDimensions());
+  console.log(table.attachChair(chair));
+}
+
+clientAbstractTableAndChairFactory(new ConcreteModernFactory());
+clientAbstractTableAndChairFactory(new ConcreteRusticFactory());
